b1558b0d7a48b58010ab5a885a43a3d4db54d014
Juliette Audema <juliette.audema@gmail.com>
2022-06-01 20:25:42 +0200

    First draft of DTR's config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,6 +39,10 @@ const IndexPage: React.FC<IndexPageProps> = ({ inputRef }) => {
     <>
       <Head>
         <title>{config.title}</title>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔦</text></svg>"
+        />
       </Head>
 
       <div className="p-8 overflow-hidden h-full border-2 rounded border-light-yellow dark:border-dark-yellow">
